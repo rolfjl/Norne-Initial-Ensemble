638996db298e0a544703e9c01dbb170133688302
rolfjl <rjl@iris.no>
2018-01-29 14:41:56 +0100

    Add files via upload

---
+++ NorneInitialEnsemble.m
@@ -0,0 +1,386 @@
+function NorneInitialEnsemble(ensembleSize, randomNumber)
+
+% This function generate an initial ensemble for the full Norne field.
+% The ensemble is generated for porosity, log-permeability, net-to-gross,
+% z-multipliers for base of layers [1,8,11,12,15,18], fault multipliers,
+% oil-water contacts, multipliers for scaling of rel-perm curves, and
+% region multipliers. Total number of generated parameters is 148159.
+%
+% For more information we refer to the paper: 
+%
+% Lorentzen, R., Luo, X., Bhakta, T., Valestrand, R.: "History matching
+% Norne reservoir and petroelastic models using seismic impedance with
+% correlated noise" Submitted to SPE Journal.
+% 
+% We also ask that the above paper is cited in publications aided by this
+% code.
+%
+% Input:
+% - ensembleSize : Number of generated ensemble members (defalult 100)
+% - randomNumber : Number used to initialize random generator 
+%                  (default 1.2345e5)
+%
+% This program is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+%
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with this program.  If not, see <http://www.gnu.org/licenses/>.
+%
+% Copyright (C): IRIS (International Research Institute of Stavanger), 2017. 
+% Contact: Rolf.Lorentzen@iris.no
+
+
+% set random generator
+if nargin < 2
+    rng(1.2345e5);
+else
+    if isscalar(randomNumber) && isreal(randomNumber) && ...
+       rem(randomNumber,1) == 0 && randomNumber >= 0
+        rng(randomNumber);
+    else
+        error('Random seed must be a nonnegative integer')
+    end
+end
+
+% number of ensemble members
+if nargin < 1
+    N = 100;
+else
+    if isscalar(ensembleSize) && isreal(ensembleSize) && ...
+       rem(ensembleSize,1) == 0 && ensembleSize > 0
+        N = ensembleSize;
+    else
+        error('Ensemble size must be a positive integer')
+    end
+end
+    
+% set geostatistical parameters and initialize
+norne = NorneGeostat();
+A = norne.actnum; % mask for active gridcells
+N_A = sum(A); % number of active gridcells
+D = norne.dim; % reservoir dimention
+N_L = D(1)*D(2); % number of gridcells in a layer
+N_F = prod(D); % total number of gridcells
+N_FP = sum([13309, 53, 3, 8, 5]); % number of free parameters: multz, multflt, multregt, kwo+kwg, owc
+N_S = 3; % number of static fields
+ensemble = zeros(N_A*N_S + N_FP,N); % initialize ensemble
+
+% mean values for poro, log-permx and ntg
+M(:,1) = norne.poroMean; 
+M(:,2) = norne.permxLogMean;
+M(:,3) = norne.ntgMean;
+
+% std for poro, permx and ntg
+S(:,1) = norne.poroStd;
+S(:,2) = norne.permxStd;
+S(:,3) = norne.ntgStd;
+
+% mean and std for multz
+A_L = reshape(A,prod(D(1:2)),D(3)); % help variable (layerwise actnum)
+M_M = [norne.z1Mean,norne.z8Mean,norne.z11Mean,norne.z12Mean,norne.z15Mean,norne.z18Mean];
+S_M = [norne.z1Std,norne.z8Std,norne.z11Std,norne.z12Std,norne.z15Std,norne.z18Std];
+
+% mean and std for multflt and multreg
+M_MF = norne.multfltLogMean;
+S_MF = norne.multfltStd;
+M_MR = norne.multregtLogMean;
+S_MR = norne.multregtStd;
+
+% bounds for krw+krg
+KR_LU = [norne.krwLB, norne.krwUB; norne.krgLB, norne.krgUB];
+
+% bounds for owc
+OWC_LU = [norne.owcLB,norne.owcUB];
+
+% mean correlation lengths (ranges)
+C(:,1) = norne.poroRange;
+C(:,2) = norne.permxRange;
+C(:,3) = norne.ntgRange;
+if isfield('norne','multzRange')
+    C_MZ = norne.multzRange;
+else
+    C_MZ = 26; 
+end
+
+% std for correlation lengths
+if isfield('norne','C_S')
+    C_S = norne.C_S;
+else
+    C_S = 2; 
+end
+
+% Poro / Permx correlation
+R1 = norne.poroPermxCorr;
+
+% Poro / NTG correlation
+R2 = norne.poroNtgCorr;
+
+% initial ensemble for Poro, Permx, NTG and Free parameters
+for I = 1:N
+    
+    % multz
+    A_MZ = A_L(:,[1 8 11 12 15 18]);
+    A_MZ = A_MZ(:);
+    N_A_MZ = sum(A_MZ);
+    B = 1; % state vector start index
+    E = N_A_MZ; % state vector end index
+    V = ones(N_L,1);
+    M_MZ = kron(M_M',V);
+    S_MZ = kron(S_M',V);
+    X = GaussianWithVariableParameters([D(1),D(2),6],M_MZ,S_MZ,C_MZ,C_S);
+    ensemble(B:E,I) = X(A_MZ==1);
+   
+    % multflt
+    N_MF = length(M_MF);
+    B = B + N_A_MZ;
+    E = E + N_MF;
+    X = M_MF + S_MF*randn(N_MF,1);
+    ensemble(B:E,I) = X;
+    
+    % multreg
+    N_MR = length(M_MR);
+    B = B + N_MF;
+    E = E + N_MR;
+    X = M_MR + S_MR*randn(N_MR,1);
+    ensemble(B:E,I) = X;
+    
+    % krw+krw
+    N_KR = 8;
+    B = B + N_MR;
+    E = E + N_KR;
+    X = [KR_LU(1,1) + (KR_LU(1,2)-KR_LU(1,1))*rand(4,1);...
+         KR_LU(2,1) + (KR_LU(2,2)-KR_LU(2,1))*rand(4,1)];
+    ensemble(B:E,I) = X;
+    
+    % owc
+    N_OWC = 5;
+    B = B + N_KR;
+    E = E + N_OWC;
+    X = OWC_LU(:,1) + (OWC_LU(:,2)-OWC_LU(:,1)).*rand(N_OWC,1);
+    ensemble(B:E,I) = X;
+    
+    % poro
+    B = B + N_OWC;
+    E = E + N_A;
+    X1 = GaussianWithVariableParameters(D,zeros(N_F,1),1,C(:,1),C_S);
+    ensemble(B:E,I) = M(:,1) + S(:,1)*X1(A==1);
+    
+    % permx
+    B = B + N_A;
+    E = E + N_A;
+    X2 = GaussianWithVariableParameters(D,zeros(N_F,1),1,C(:,2),C_S);
+    X = R1*X1 + sqrt(1-R1^2)*X2;
+    ensemble(B:E,I) = M(:,2) + S(:,2)*X(A==1);
+    
+    % ntg 
+    B = B + N_A;
+    E = E + N_A;
+    X2 = GaussianWithVariableParameters(D,zeros(N_F,1),1,C(:,3),C_S);
+    X = R2*X1 + sqrt(1-R2^2)*X2;
+    ensemble(B:E,I) = M(:,3) + S(:,3)*X(A==1);
+   
+end
+
+% Define bounds
+G = ones(N_A,1);
+F_LB = [norne.zLB*ones(N_A_MZ,1); norne.multfltLB*ones(N_MF,1); norne.multregtLB*ones(N_MR,1);...
+    KR_LU(1,1)*ones(4,1); KR_LU(2,1)*ones(4,1); norne.owcLB];
+F_UB = [norne.zUB*ones(N_A_MZ,1); norne.multfltUB*ones(N_MF,1); norne.multregtUB*ones(N_MR,1);...
+    KR_LU(1,2)*ones(4,1); KR_LU(2,2)*ones(4,1); norne.owcUB];
+S_LB = [norne.poroLB*G;norne.permxLB*G;norne.ntgLB*G];
+S_UB = [norne.poroUB*G;norne.permxUB*G;norne.ntgUB*G];
+
+% Adjust parameters with bounds
+N_S_G = N_S*N_A;
+ensemble(1:N_FP,:) = AdjustVariableWithInBounds(ensemble(1:N_FP,:),F_LB,F_UB);
+ensemble(N_FP+1:N_FP+N_S_G,:) = AdjustVariableWithInBounds(ensemble(N_FP+1:N_FP+N_S_G,:),S_LB,S_UB); %#ok<*NASGU>
+
+% Save the ensemble
+save('NorneInitEns.mat', 'ensemble');
+
+
+%-----------------------------------------------
+% Subfunction 
+%----------------------------------------------- 
+function [x,corrLength] = GaussianWithVariableParameters(fieldDim,meanValue, ...
+			  Sdev,meanCorrLength,stdCorrLength)
+
+% Setup a gaussian field with correlation length drawn from a
+% normal distribution. The horizontal layers are genereated
+% independently
+%
+% Input:
+% - fieldDim       : dimension of the field.
+% - meanValue      : the mean value of the field (vector of the size
+%                    of the field).
+% - Sdev           : standard deviation of the field.
+% - meanCorrLength : mean correlation length.
+% - stdCorrLength  : standard deviation of the correlation length.
+%
+% Output:
+% - x              : the generated field.
+% - corrLenght     : the drawn correlation length.
+
+corrLength=AddGnoise(meanCorrLength,stdCorrLength,1);
+if length(fieldDim)<3
+    
+    x=meanValue+FastGaussian(fieldDim,Sdev,corrLength);
+    
+else
+    
+    layerDim=prod(fieldDim(1:2));
+    
+    % Initialization
+    x=meanValue;
+    if length(Sdev)==1
+        for I=1:fieldDim(3)
+            x(1+(I-1)*layerDim:I*layerDim,1)=meanValue(1+(I-1)*layerDim: ...
+                I*layerDim)+FastGaussian(fieldDim(1:2),Sdev,corrLength);
+            
+            % Generate new correlation length for the next layer
+            corrLength=AddGnoise(meanCorrLength,stdCorrLength,1);
+        end
+    else
+        for I=1:fieldDim(3)
+            x(1+(I-1)*layerDim:I*layerDim,1)=meanValue(1+(I-1)*layerDim: ...
+                I*layerDim)+ ...
+                FastGaussian(fieldDim(1:2),Sdev(1+(I-1)*layerDim:I* ...
+                layerDim),corrLength);
+            
+            % Generate new correlation length for the next layer
+            corrLength=AddGnoise(meanCorrLength,stdCorrLength,1);
+        end
+    end
+    
+end
+
+
+%-----------------------------------------------
+% Subfunction 
+%----------------------------------------------- 
+function [variable,n] = AdjustVariableWithInBounds(variable, ...
+						lowerbound,upperbound)
+
+% The function returns variable such that 
+%    lowerbound <= variable(i) <= upperbound,
+% i.e. if variable(i)<lowerbound before calling the function, then
+% variable(i)=lowerbound as output. Similarly for upperbound, both
+% with inequality the opposite way. If there is no
+% lowerbound/upperbound, set the corresponding matrix empty.
+%
+% Input:
+% - variable   : Vector or matrix. Variables (or an ensemble of variable 
+%                samples) to be checked and truncated (if necessary) 
+% lowerbound   : Scalar or vector. Lower bound(s) for the variables to be
+%                checked
+% upperbound   : Scalar or vector. Upper bound(s) for the variables to be
+%                checked
+% Output:
+% - variable   : Variables after check/truncation
+% - n          : Number of truncations
+
+if nargin < 3
+  error(['adjustVariableWithInBounds is only implemented for three' ...
+	 ' arguments'])
+end
+
+ne=size(variable,2);
+n=0;
+if ~isempty(lowerbound)
+    if max(size(lowerbound))==1
+        n=sum(variable<lowerbound);
+        variable(variable<lowerbound)=lowerbound;
+    else
+        lowerbound=kron(lowerbound,ones(1,size(variable,2)));
+        n=sum(variable<lowerbound);
+        variable(variable<lowerbound)=lowerbound(variable<lowerbound);
+    end
+end
+
+if ~isempty(upperbound)
+    if max(size(upperbound))==1
+        n=n+sum(variable>upperbound);
+        variable(variable>upperbound)=upperbound;
+    else
+        upperbound=kron(upperbound,ones(1,ne));
+        n=n+sum(variable>upperbound);
+        variable(variable>upperbound)=upperbound(variable>upperbound);
+    end
+end
+
+
+%-----------------------------------------------
+% Subfunction 
+%----------------------------------------------- 
+function [Y,RTSIGMA] = AddGnoise( Ytrue,SIGMA,SQ )
+
+% Add noise, normally distributed, with covariance given by SIGMA.
+%
+% Input:
+% - Ytrue    Original signal.
+% - SIGMA    Specification of covariance matrix of noise.  May be
+%            entered as scalar, vector or full matrix. If it SIGMA
+%            is a vector, then it is interpreted as the covariance
+%            matrix is diag(SIGMA).
+% - SQ       If present, determine whether SIGMA or SIGMA*SIGMA' is used
+%            as the covariance matrix.  Thus, if the square root of
+%            the covariance matrix has allready been calculated
+%            previously, work may be saved by setting SQ to 1.
+%
+% Output:
+% - Y        Signal with noise added.
+% - RTSIGMA  The square root of SIGMA; RTSIGMA*RTSIGMA' = SIGMA.
+%            (Helpful if it is cumbersome to compute).
+
+% Compute the normally distributed noise, with covariance matrix
+% SIGMA or SIGMA*SIGMA'.
+try
+    
+    if nargin > 2 && SQ == 1
+        % Use SIGMA*SIGMA' as covariance matrix
+        RTSIGMA = SIGMA ;
+        if min(size(SIGMA)) == 1
+            % SIGMA is a scalar or vector
+            error = RTSIGMA.*randn(size(Ytrue)) ;
+        else
+            error = RTSIGMA*randn(size(RTSIGMA,2),1) ;
+        end
+    else
+        % Use SIGMA as covariance matrix
+        if min(size(SIGMA))==1
+            % SIGMA is entered as a scalar or a vector
+            RTSIGMA = realsqrt(SIGMA);
+            error = RTSIGMA.*randn(size(Ytrue));
+        else
+            [RTSIGMA,p] = chol(SIGMA); % The matrix must be transposed.
+            if p>0
+                disp('Problem with Cholesky factorization')
+                disp(['p = ',num2str(p)]);
+                RTSIGMA = real(sqrtm(SIGMA));
+                disp('Finnaly - we got a square root!')
+            end
+            RTSIGMA = RTSIGMA';
+            error = RTSIGMA*randn(size(Ytrue));
+        end %if
+    end
+    % Add the noise:
+    Y = Ytrue+error;
+  
+catch err
+    
+  disp('Error in AddGnoise');
+  disp('Size Ytrue:')
+  size(Ytrue)
+  disp('Size SIGMA:')
+  size(SIGMA)
+  rethrow(err);
+  
+end
+
